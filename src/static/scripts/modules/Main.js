48eff69738b2564b70c51a11cfbdbf82db428ddf
danjonesdev <dannyjones360@gmail.com>
2022-02-04 15:37:15 +0000

    Minors

--- src/static/scripts/modules/Main.js
+++ src/static/scripts/modules/Main.js
@@ -22,7 +22,7 @@ const Main = {
   render() {
     const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
-    Main.wrapper.style.height = window.screen.availHeight;
+    Main.wrapper.style.height = `${window.screen.availHeight}px`;
 
     Audio.setContext();
     Effects.render();
