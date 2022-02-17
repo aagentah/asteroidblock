1ffe93bfeb65e3c016349f18038a5fa644b4f4e3
danjonesdev <dannyjones360@gmail.com>
2022-02-17 14:53:52 +0000

    Minors

--- src/static/scripts/modules/Audio.js
+++ src/static/scripts/modules/Audio.js
@@ -72,7 +72,23 @@ const Audio = {
     }
   },
 
+  unlockAudioContext(audioCtx) {
+    if (audioCtx.state !== 'suspended') return;
+    const b = document.body;
+    const events = ['touchstart', 'touchend', 'mousedown', 'keydown'];
+    events.forEach(e => b.addEventListener(e, unlock, false));
+    function unlock() {
+      audioCtx.resume().then(clean);
+    }
+    function clean() {
+      events.forEach(e => b.removeEventListener(e, unlock));
+    }
+  },
+
   play: async notes => {
+    const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    Audio.unlockAudioContext(audioCtx);
+
     let effectParams = {};
     let effect, param;
 
